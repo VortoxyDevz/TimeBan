8492136cee2d5474ad2f103cc9842796e07a8212
Tyler James McPherson <tylergamesleet@gmail.com>
2016-03-21 22:38:05 -0500

    Added src and Main.php

---
+++ src/Main.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace TimeBan
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+use pocketmine\permission\BanEntry;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerPreLoginEvent;
+
+class TimeBan extends PluginBase implements Listener{
+}

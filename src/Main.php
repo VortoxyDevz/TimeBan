83ca8ed1a3ee514ea720a74788c3eb33ffd431e4
applqpak <applqpak@gmail.com>
2016-03-22 14:50:28 +1100

    Update Main.php

--- src/Main.php
+++ src/Main.php
@@ -1,14 +1,50 @@
 <?php
 
-namespace TimeBan
+  namespace TimeBan;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\Player;
-use pocketmine\permission\BanEntry;
-use pocketmine\utils\Config;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerPreLoginEvent;
+  use pocketmine\plugin\PluginBase;
+  use pocketmine\event\Listener;
+  use pocketmine\event\player\PlayerPreLoginEvent;
+  use pocketmine\command\Command;
+  use pocketmine\command\CommandSender;
+  use pocketmine\utils\TextFormat as TF;
+  use pocketmine\utils\Config;
+  use pocketmine\Player;
 
-class TimeBan extends PluginBase implements Listener{
-}
+  class Main extends PluginBase implements Listener
+  {
+
+    public function dataPath()
+    {
+
+      return $this->getDataFolder();
+
+    }
+
+    public function server()
+    {
+
+      return $this->getServer();
+
+    }
+
+    public function onEnable()
+    {
+
+      $this->server()->getPluginManager()->registerEvents($this, $this);
+
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, $label, array $args)
+    {
+
+    }
+
+    public function onPreLogin(PlayerPreLoginEvent $event)
+    {
+
+    }
+
+  }
+
+?>
